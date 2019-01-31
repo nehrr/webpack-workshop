eb7001b128f2d51d6b6ce740c134abe6f96d14c7
ERNOUL <iamkapitaan@gmail.com>
2019-01-31 14:42:09 +0100

    exclude node_modules from loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const HTMLWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   mode: process.env.NODE_ENV || "production",
   module: {
     rules: [
-      { test: /\.(js|jsx)$/, use: "babel-loader" },
+      { test: /\.(js|jsx)$/, exclude: /node_modules/, use: "babel-loader" },
       { test: /\.css$/, use: ["style-loader", "css-loader"] },
       {
         test: /\.html$/,
